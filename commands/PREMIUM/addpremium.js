5bcc4f5380e7379d333a0dfa9b80a50dd6526417
tserio123 <91136162+tserio123@users.noreply.github.com>
2021-12-17 08:47:06 +0530

    Update addpremium.js

--- commands/PREMIUM/addpremium.js
+++ commands/PREMIUM/addpremium.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "addpremium",
     run : async (client,message,args)=> {
 
-if(message.author.id !== "921042662386176011) return;
+if(message.author.id !== "921042662386176011") return;
 
 let guild = client.guilds.cache.get(args[0])
 
